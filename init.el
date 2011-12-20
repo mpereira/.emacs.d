5fe4e0f96fa6b406d29d23c550d3725d60ae7dbe
Murilo Pereira <murilo@murilopereira.com>
2011-12-20 01:14:08 -0200

    Lose the UI.

--- init.el
+++ init.el
@@ -15,6 +15,11 @@
 (require 'paredit)
 (paredit-mode t)
 
+;; Lose the UI.
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
 ;; Quiel startup.
 (setq inhibit-startup-echo-area-message t)
 (setq inhibit-startup-message t)
