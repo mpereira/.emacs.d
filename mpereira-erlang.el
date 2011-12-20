d0dd3bfc8fd041d829c077d0043396e8c3b31067
Murilo Pereira <murilo@murilopereira.com>
2011-12-20 01:14:52 -0200

    Pareditize erlang code.

--- mpereira-erlang.el
+++ mpereira-erlang.el
@@ -6,6 +6,16 @@
 (require 'erlang-start)
 (require 'erlang-flymake)
 
+(add-hook
+ 'erlang-extended-mode-hook
+ (lambda ()
+    (define-key erlang-extended-mode-map [?\(] 'paredit-open-parenthesis)
+    (define-key erlang-extended-mode-map [?\[] 'paredit-open-square)
+    (define-key erlang-extended-mode-map [?\{] 'paredit-open-curly)
+    (define-key erlang-extended-mode-map [?\)] 'paredit-close-parenthesis)
+    (define-key erlang-extended-mode-map [?\}] 'paredit-close-curly)
+    (define-key erlang-extended-mode-map [?\]] 'paredit-close-square)))
+
 (add-to-list 'load-path "~/.emacs.d/packages/distel/elisp")
 (require 'distel)
 (distel-setup)
