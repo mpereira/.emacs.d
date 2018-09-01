908c6752404baa77eedc0acfaa6ae2f2e3a93d16
Murilo Pereira <murilo@murilopereira.com>
2018-09-02 01:16:44 +0200

    Marmalade has been deprecated.

--- init.el
+++ init.el
@@ -7,8 +7,7 @@
 
 (setq package-archives '(("org" . "http://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+                         ("melpa" . "http://melpa.org/packages/")))
 
 (unless package-archive-contents
   (package-refresh-contents))
