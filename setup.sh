f2647c683e5bbe59560ba7ba03b601200afcf71a
Murilo Pereira <murilo@murilopereira.com>
2020-05-29 12:44:10 +0200

    Enable LSP for Clojure.

--- setup.sh
+++ setup.sh
@@ -6,4 +6,9 @@ set -euxo pipefail
 
 rustup component add rustfmt
 
+curl https://github.com/snoe/clojure-lsp/releases/download/release-20200514T134144/clojure-lsp \
+     -L  \
+     -o /usr/local/bin/clojure-lsp
+chmod 755 /usr/local/bin/clojure-lsp
+
 python3 -m pip install mypy flake8 pylint black
