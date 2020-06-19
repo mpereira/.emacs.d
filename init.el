3164d09cd71c41fd7191d21dd21911c1d0bdb4b7
Murilo Pereira <murilo@murilopereira.com>
2020-06-20 00:04:55 +0200

    Some performance-related changes.

--- init.el
+++ init.el
@@ -1,11 +1,28 @@
 (setq debug-on-error t)
-(setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'after-init-hook
-          (lambda ()
-            (garbage-collect)
-            (setq debug-on-error nil)
-            (setq gc-cons-threshold 100000000))) ;; 100MB
 
+(setq mpereira/gc-cons-threshold-original gc-cons-threshold)
+(setq mpereira/gc-cons-percentage-original gc-cons-percentage)
+
+(lexical-let ((original-gc-cons-threshold gc-cons-threshold)
+              (original-gc-cons-percentage gc-cons-percentage)
+              (normal-gc-cons-threshold (* 512 1024 1024))
+              (normal-gc-cons-percentage 0.1)
+              (initial-gc-cons-threshold most-positive-fixnum)
+              (initial-gc-cons-percentage 0.2))
+  (setq gc-cons-threshold initial-gc-cons-threshold)
+  (setq gc-cons-percentage initial-gc-cons-percentage)
+  (add-hook 'after-init-hook
+            (lambda ()
+              (garbage-collect)
+
+              (setq debug-on-error nil)
+
+              (setq mpereira/gc-cons-threshold-normal normal-gc-cons-threshold)
+              (setq mpereira/gc-cons-threshold-maximum #x40000000)
+              (setq mpereira/gc-cons-percentage-normal normal-gc-cons-percentage)
+              (setq mpereira/gc-cons-percentage-maximum normal-gc-cons-percentage)
+
+              (setq gc-cons-threshold normal-gc-cons-threshold))))
 (require 'package)
 
 (package-initialize)
