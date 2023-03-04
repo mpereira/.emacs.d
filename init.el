c5b1ed56f95588ffb5da26d8127a5bea77722a56
Murilo Pereira <murilo@murilopereira.com>
2023-03-04 21:55:20 +0100

    Some GC-related changes
    Make it simpler.

--- init.el
+++ init.el
@@ -2,24 +2,17 @@
 
 (setq debug-on-error t)
 
-(setq mpereira/gc-cons-percentage-original gc-cons-percentage)
-(setq mpereira/gc-cons-threshold-original gc-cons-threshold)
-
-(let ((gc-cons-percentage-initial 0.2)
-      (gc-cons-percentage-normal 0.1)
-      (gc-cons-threshold-initial most-positive-fixnum)
-      (gc-cons-threshold-normal (* 512 1024 1024)))
+(let* ((gc-cons-percentage-original gc-cons-percentage)
+       (gc-cons-threshold-original gc-cons-threshold)
+       (gc-cons-percentage-initial 0.6)
+       (gc-cons-threshold-initial most-positive-fixnum))
   (setq gc-cons-threshold gc-cons-threshold-initial)
   (setq gc-cons-percentage gc-cons-percentage-initial)
   (add-hook 'after-init-hook
             (lambda ()
               (garbage-collect)
-              (setq mpereira/gc-cons-percentage-maximum gc-cons-percentage-normal)
-              (setq mpereira/gc-cons-percentage-normal gc-cons-percentage-normal)
-              (setq mpereira/gc-cons-threshold-maximum #x40000000)
-              (setq mpereira/gc-cons-threshold-normal gc-cons-threshold-normal)
-              (setq gc-cons-threshold gc-cons-threshold-normal)
-              (setq gc-cons-percentage gc-cons-percentage-normal)
+              (setq gc-cons-threshold gc-cons-threshold-original)
+              (setq gc-cons-percentage gc-cons-percentage-original)
               (setq debug-on-error nil))))
 
 (require 'package)
